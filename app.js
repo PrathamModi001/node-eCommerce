58cbf6c536f1a588e8d44cead2488b3801a6dfc1
PrathamModi001 <prathammodi001@gmail.com>
2022-12-24 18:05:44 +0530

    Revisit after a month

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 const path = require('path');
 
 const express = require('express');
-const bodyParser = require('body-parser');
-
 const app = express();
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.set('view engine', 'ejs');
+app.set('views', 'views');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminData.routes);
 app.use(shopRoutes);
