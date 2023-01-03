826d5e062254ecefacffb43af954693ffe7b95f6
PrathamModi001 <prathammodi001@gmail.com>
2023-01-04 01:50:42 +0530

    Added MVC architechture

--- app.js
+++ app.js
@@ -1,25 +1,24 @@
 const path = require('path');
 
 const express = require('express');
-const app = express();
-
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+
+const errorController = require('./controllers/error');
+
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use('/admin', adminData.routes);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found' });
-});
+app.use(errorController.get404);
 
-app.listen(3000 , () => {
-  console.log("Listening on port 3000")
-});
+app.listen(3000);
