ee7f5fc5988041cccb6391ec573f436c01c6e4e5
PrathamModi001 <prathammodi001@gmail.com>
2023-01-04 01:49:56 +0530

    Added the MVC architechture.

--- app.js
+++ app.js
@@ -20,4 +20,6 @@ app.use((req, res, next) => {
   res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
-app.listen(3000);
+app.listen(3000 , () => {
+  console.log("Listening on port 3000")
+});
