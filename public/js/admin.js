38a12faad48fc405838f654e2c3b419a24489fbc
PrathamModi001 <prathammodi001@gmail.com>
2023-03-30 00:12:15 +0530

    delete prods via async requests

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,5 +1,23 @@
 const deleteProduct = (btn) => {
     const productId = btn.parentNode.querySelector('[name=productId]').value;
     const csrf = btn.parentNode.querySelector('[name=_csrf]').value;
-    console.log(csrf);
+
+    // through the fetch method we can delete successfully, but we have to update the page to remove the card. To do that on the go, we will REMOVE the card (article tag wala section from ejs) when we click the button i.e. execute this js file.
+    // so we need to select the article.
+    const productElement = btn.closest('article');
+    
+    fetch('/admin/product/' + productId, {
+        method: 'DELETE',
+        headers: {
+            'csrf-token': csrf
+        }
+    })
+        .then(result => {return result.json()})
+        .then(data => {
+            // console.log(data)
+            // productElement.remove(); not supported for old so: 
+            productElement.parentNode.removeChild(productElement);
+        })
+        
+        .catch(err => console.log(err))
 }
